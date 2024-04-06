4356634537266ba66301926ed8d938e77816411c
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-06 12:00:49 -0700

    set up /getIncidents route

--- server/server.py
+++ server/server.py
@@ -9,6 +9,9 @@ supabase = create_supabase_client()
 async def index():
   return "Welcome to LeGrandTheftAuto"
 
+@app.get("/getIncidents")
+async def get_incidents():
+   # get all incidents from the incidents table
 
 if __name__ == "__main__":
     uvicorn.run("server:app", port=8000, reload=True)
