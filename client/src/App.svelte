197b8832ba33f1b5fa396511f3f8afc13522315b
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-06 14:51:04 -0700

    setup tailwind + daisyUI + basic layout header

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,18 @@
 <script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Header from "./lib/Header.svelte";
+  import Home from "./lib/Home.svelte";
+  import About from "./lib/About.svelte";
 
+  export let url = "";
 </script>
 
-<main>
-  
-</main>
+<Router {url}>
+  <Header />
+  <div>
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+</Router>
 
 
