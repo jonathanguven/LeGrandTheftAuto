1cf2ccaf5a9590d10af5e009eccff0a45aff0d46
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-06 14:56:28 -0700

    remove /lib from gitignore

---
+++ client/src/lib/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <Link to="/" class="btn btn-ghost text-xl">LeGrandTheftAuto</Link>
+  </div>
+  <div class="flex-none">
+    <div class="flex-1">
+      <Link to="/about" class="btn btn-ghost text-lg">About</Link>
+    </div>
+  </div>
+</div>
