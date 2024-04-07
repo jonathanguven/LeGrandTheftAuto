bb5af8c12bbbb0249eb015861cd4f5a7000b1b4b
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-07 08:52:07 -0700

    move url to env

--- client/src/apiFunctions/getGeoJson.js
+++ client/src/apiFunctions/getGeoJson.js
@@ -5,7 +5,7 @@ export const getGeoJson = async (weeks_back) => {
     if (weeks_back) {
       num_weeks = weeks_back;
     }
-    const response = await fetch(`http://localhost:8000/geojson?weeks_back=${num_weeks}`);
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/geojson?weeks_back=${num_weeks}`);
     const data = await response.json();
     return data;
   } catch (error) {
