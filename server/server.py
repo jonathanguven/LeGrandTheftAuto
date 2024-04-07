c860489c33721082343f9b412773272fed6106c6
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-06 17:08:24 -0700

    CORS fix for fastapi endpoint

--- server/server.py
+++ server/server.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn 
 from db.supabase import create_supabase_client
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 supabase = create_supabase_client()
 
 @app.get("/")
