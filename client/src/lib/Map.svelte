ab8fddbe092cb78df5f8fa6651bbc14b55e57f70
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-06 22:07:36 -0700

    change data to 2 months

--- client/src/lib/Map.svelte
+++ client/src/lib/Map.svelte
@@ -30,20 +30,9 @@
     lat = map.getCenter().lat;
   }
 
-  onMount(async () => {
-    data = await getGeoJson();
-    console.log(data);
-    const initialState = { lng: lng, lat: lat, zoom: zoom };
-
-    map = new Map({
-      container: mapContainer,
-      accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
-      style,
-      center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom,
-    });
-
-    map.on('load', () => {
+  async function addDataLayer() {
+    if (!map.getSource('incidents')) {
+      if (!data) data = await getGeoJson();
       map.addSource('incidents', {
         type: 'geojson',
         data: data
@@ -58,11 +47,31 @@
           'circle-color': '#B42222'
         }
       });
-    })
+    }
+  }
+
+  onMount(async () => {
+    data = await getGeoJson();
+
+    const initialState = { lng: lng, lat: lat, zoom: zoom };
+
+    map = new Map({
+      container: mapContainer,
+      accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
+      style,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+
+    map.on('load', addDataLayer);
+
+    map.on('styledata', async () => {
+      await addDataLayer(); 
+    });
+
     map.on('move', () => {
       updateData();
     });
-
   });
 
   onDestroy(() => {
@@ -78,7 +87,7 @@
 <div class="sidebar">
   Longitude: {lng.toFixed(4)} | Latitude: {lat.toFixed(4)} | Zoom: {zoom.toFixed(2)}
 </div>
-<!-- <input type="checkbox" class="toggle mode" bind:checked={dark}/> -->
+<input type="checkbox" class="toggle mode" bind:checked={dark}/>
 <div class="w-full h-full absolute">
   <div class="w-full h-full" bind:this={mapContainer} />
 </div>
--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn 
 from db.supabase import create_supabase_client
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
+from dateutil import relativedelta
 
 app = FastAPI()
 app.add_middleware(
@@ -50,29 +50,24 @@ async def get_geojson():
     except (TypeError, ValueError):
       continue
   geojson = {
-    "type": "GeoJSON",
+    "type": "FeatureCollection",
     "features": features
   }
   return geojson
 
 def get_last_month_incidents():
-  # Specify the columns you want to fetch
   columns = 'Incident Date, Incident Time, Latitude, Longitude, Incident ID, Incident Description, Analysis Neighborhood, Incident Subcategory, Incident Datetime'
 
-  # Calculate the date one month back from today
-  one_month_back = datetime.now() - relativedelta(months=1)
+  num_months_back = datetime.now() - relativedelta(months=2)
   
-  # Format the date as string in 'YYYY-MM-DD' format (adjust as necessary)
-  formatted_date = one_month_back.strftime('%Y/%m/%d')
+  formatted_date = num_months_back.strftime('%Y/%m/%d')
   formatted_end_date = datetime.now().strftime('%Y/%m/%d')
 
-  # Execute the query with a filter for dates within the last month
-  # Assuming 'Incident Date' is stored in a format that Supabase can directly compare (e.g., 'YYYY-MM-DD')
   data, count = supabase.table('incidents')\
-                            .select(columns)\
-                            .gte('Incident Date', formatted_date)\
-                            .lte('Incident Date', formatted_end_date)\
-                            .execute()
+                        .select(columns)\
+                        .gte('Incident Date', formatted_date)\
+                        .lte('Incident Date', formatted_end_date)\
+                        .execute()
   return data[1]
 
 if __name__ == "__main__":
