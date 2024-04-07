6909bfb4878cf20f759c15ba2165027236e5857e
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-07 02:23:41 -0700

    implemented buttons to switch displayed layer

--- client/src/lib/Map.svelte
+++ client/src/lib/Map.svelte
@@ -5,18 +5,12 @@
   import { getGeoJson } from '../apiFunctions/getGeoJson';
 
   let data;
-
   let dark = true;
+  // let layer = 'incidents';
 
   $: theme = dark ? 'dark' : 'light';
   $: color = dark ? '#87CEEB' : '#B42222';
   $: style = `mapbox://styles/mapbox/${theme}-v11`;
-
-  $: {
-    if (map) {
-      map.setStyle(style);
-    }
-  }
 
   let map;
   let mapContainer;
@@ -39,13 +33,14 @@
         type: 'geojson',
         data: data
       });
-
+    }
+    if (!map.getLayer('incidents')) {
       map.addLayer({
         id: 'incidents',
         type: 'circle',
         source: 'incidents',
         paint: {
-          'circle-radius': 6,
+          'circle-radius': 3,
           'circle-color': `${color}`
         }
       });
@@ -53,113 +48,124 @@
   }
 
   async function addHeatMapLayer() {
-    map.addSource('heatSource', {
-      type: 'geojson',
-      data: data
-    });
+    if (!map.getSource('heatSource')) {
+      map.addSource('heatSource', {
+        type: 'geojson',
+        data: data
+      });
+    }
     
-    map.addLayer({
-      id: 'heatIncidents',
-      type: 'heatmap',
-      source: 'heatSource',
-      maxzoom: 15,
-      paint: {
-        'heatmap-weight': {
-          type: 'exponential',
-          stops: [
-            [1,0.2],
-            [62, 1]
-          ]
-        },
-        //increase intensity as zoom level increases
-        'heatmap-intensity': {
-          stops: [
-            [11,1],
-            [15,3]
-          ]
-        },
-        //assign color values be applied to points depending on their density
-        'heatmap-color': [
-          'interpolate',
-          ['linear'],
-          ['heatmap-density'],
-          0,
-          'rgba(236,222,239,0)',
-          0.2,
-          'rgb(200, 0, 10)',
-          0.4,
-          'rgb(220, 0, 20)',
-          0.6,
-          'rgb(230, 0, 40)',
-          0.8,
-          'rgb(255, 0, 99)'
-        ],
-        //increase radius as zoom increases
-        'heatmap-radius': {
-          stops: [
-            [11,10],
-            [15,18]
-          ]
-        },
-        //decrease opacity to transition into the circle layer
-        'heatmap-opacity': {
-          default: 1,
-          stops: [
-            [14,1],
-            [15,0]
-          ]
+    if (!map.getLayer('heatIncidents')) {
+      map.addLayer({
+        id: 'heatIncidents',
+        type: 'heatmap',
+        source: 'heatSource',
+        maxzoom: 15,
+        paint: {
+          'heatmap-weight': {
+            type: 'exponential',
+            stops: [
+              [1,0.2],
+              [62, 1]
+            ]
+          },
+          //increase intensity as zoom level increases
+          'heatmap-intensity': {
+            stops: [
+              [11,1],
+              [15,3]
+            ]
+          },
+          //assign color values be applied to points depending on their density
+          'heatmap-color': [
+            'interpolate',
+            ['linear'],
+            ['heatmap-density'],
+            0,
+            'rgba(236,222,239,0)',
+            0.2,
+            'rgb(200, 0, 10)',
+            0.4,
+            'rgb(220, 0, 20)',
+            0.6,
+            'rgb(230, 0, 40)',
+            0.8,
+            'rgb(255, 0, 99)'
+          ],
+          //increase radius as zoom increases
+          'heatmap-radius': {
+            stops: [
+              [11,10],
+              [15,18]
+            ]
+          },
+          //decrease opacity to transition into the circle layer
+          'heatmap-opacity': {
+            default: 1,
+            stops: [
+              [14,1],
+              [15,0]
+            ]
+          }
         }
-      }
-    });
-
-    map.addLayer({
-      id: 'incidents2',
-      type: 'circle',
-      source: 'incidents',
-      minzoom: 14,
-      paint: {
-        // increase the radius of the circle as the zoom level and dbh value increases
-        'circle-radius': {
-          property: 'dbh',
-          type: 'exponential',
-          stops: [
-            [{ zoom: 15, value: 1 }, 5],
-            [{ zoom: 15, value: 62 }, 10],
-            [{ zoom: 22, value: 1 }, 20],
-            [{ zoom: 22, value: 62 }, 50]
-          ]
-        },
-        'circle-color': {
-          property: 'dbh',
-          type: 'exponential',
-          stops: [
-            [0, 'rgba(236,222,239,0)'],
-            [10, 'rgb(236,222,239)'],
-            [20, 'rgb(208,209,230)'],
-            [30, 'rgb(166,189,219)'],
-            [40, 'rgb(103,169,207)'],
-            [50, 'rgb(28,144,153)'],
-            [60, 'rgb(1,108,89)']
-          ]
-        },
-        'circle-stroke-color': 'white',
-        'circle-stroke-width': 1,
-        'circle-opacity': {
-          stops: [
-            [14, 0],
-            [15, 1]
-          ]
+      });
+  
+      map.addLayer({
+        id: 'heatIncidentsButtons',
+        type: 'circle',
+        source: 'heatSource',
+        minzoom: 14,
+        paint: {
+          // increase the radius of the circle as the zoom level and dbh value increases
+          'circle-radius': {
+            property: 'dbh',
+            type: 'exponential',
+            stops: [
+              [{ zoom: 15, value: 1 }, 5],
+              [{ zoom: 15, value: 62 }, 10],
+              [{ zoom: 22, value: 1 }, 20],
+              [{ zoom: 22, value: 62 }, 50]
+            ]
+          },
+          'circle-color': {
+            property: 'dbh',
+            type: 'exponential',
+            stops: [
+              [0, 'rgba(236,222,239,0)'],
+              [10, 'rgb(236,222,239)'],
+              [20, 'rgb(208,209,230)'],
+              [30, 'rgb(166,189,219)'],
+              [40, 'rgb(103,169,207)'],
+              [50, 'rgb(28,144,153)'],
+              [60, 'rgb(1,108,89)']
+            ]
+          },
+          'circle-stroke-color': 'white',
+          'circle-stroke-width': 1,
+          'circle-opacity': {
+            stops: [
+              [14, 0],
+              [15, 1]
+            ]
+          }
         }
-      }
-    });
-    map.on('click', 'incidents2', (event) => {
-      new mapboxGl.Popup()
-        .setLngLat(event.features[0].geometry.coordinates)
-        .setHTML(`<strong>incident:</strong> ${event.features[0].properties.description} <br> <strong>date of incident: </strong>${event.features[0].properties.date}`)
-        .addTo(map);
-    });
+      });
+    }
   }
 
+  function updateVisibility(layer) {
+    map.setLayoutProperty('incidents', 'visibility', 'none');
+    map.setLayoutProperty('heatIncidents', 'visibility', 'none');
+    map.setLayoutProperty('heatIncidentsButtons', 'visibility', 'none');
+    map.setLayoutProperty(layer, 'visibility', 'visible');
+    console.log(layer);
+    console.log(map.getLayoutProperty(layer, 'visibility'));
+  }
+
+  $: {
+    if (map) {
+      map.setStyle(style);    }
+  }
 
   onMount(async () => {
     data = await getGeoJson();
@@ -174,16 +180,20 @@
       zoom: initialState.zoom,
     });
     
-    // plot layer
-    map.on('load', addDataLayer);
-    map.on('styledata', async () => {
+    map.on('load', async() => { 
       await addDataLayer(); 
-    });
-    
-    // heat layer
-    map.on('load', addHeatMapLayer());
-    map.on('styledata', async () => {
       await addHeatMapLayer();
+    });
+    map.on('styledata', async () => { 
+      await addDataLayer(); 
+      await addHeatMapLayer();
+    });
+
+    map.on('click', 'heatIncidents', (event) => {
+      new mapboxGl.Popup()
+        .setLngLat(event.features[0].geometry.coordinates)
+        .setHTML(`<strong>incident:</strong> ${event.features[0].properties.description} <br> <strong>date of incident: </strong>${event.features[0].properties.date}`)
+        .addTo(map);
     });
     
     map.on('move', () => {
@@ -196,17 +206,23 @@
   });
 </script>
 
-<head>
-
-</head>
-
-<div class="sidebar">
-  Longitude: {lng.toFixed(4)} | Latitude: {lat.toFixed(4)} | Zoom: {zoom.toFixed(2)}
-</div>
-<input type="checkbox" class="toggle mode" bind:checked={dark}/>
-<div class="w-full h-full absolute">
-  <div class="w-full h-full" bind:this={mapContainer} />
-</div>
+<main>
+  <!-- lat long description -->
+  <div class="sidebar">
+    Longitude: {lng.toFixed(4)} | Latitude: {lat.toFixed(4)} | Zoom: {zoom.toFixed(2)}
+  </div>
+  <!-- dark mode toggle -->
+  <input type="checkbox" class="toggle mode" bind:checked={dark}/>
+  <!-- layer input -->
+  <div class="buttons">
+    <button class="btn mr-4" on:click={() => updateVisibility('incidents')}>Points</button>
+    <button class="btn mr-4" on:click={() => updateVisibility('heatIncidents')}>HeatMap</button>
+  </div>
+  <!-- map container -->
+  <div class="w-full h-full absolute -mt-20">
+    <div class="w-full h-full" bind:this={mapContainer} />
+  </div>
+</main>
 
 <style>
   .sidebar {
@@ -227,5 +243,12 @@
     right: 0;
     z-index: 1;
     margin: 18px;
+  }
+  .buttons {
+    position: relative;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    margin: 16px;
   }
 </style>
