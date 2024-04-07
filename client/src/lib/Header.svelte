ef7711b064b385c49d59b30ca17410181066fcbd
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-06 22:49:30 -0700

    implemented about page

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -4,11 +4,11 @@
 
 <div class="navbar bg-base-200 p-4">
   <div class="flex-1">
-    <Link to="/" class="btn btn-ghost text-xl">LeGrandTheftAuto</Link>
+    <Link to="/" class="btn text-xl btn-ghost">LeGrandTheftAuto</Link>
   </div>
   <div class="flex-none">
     <div class="flex-1">
-      <Link to="/about" class="btn btn-ghost text-lg">About</Link>
+      <Link to="/about" class="btn text-lg btn-ghost">About</Link>
     </div>
   </div>
 </div>
